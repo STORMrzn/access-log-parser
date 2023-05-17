9e7ab9cf0f7a01744df8ccb7bdcce42b3271b566
Alexandr Suzdaltsev <ASuzdaltsev@inno.tech>
2023-05-17 22:23:27 +0300

    Changed code to simple calc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+    public static final String RED = "\u001B[31m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String RESET = "\u001B[0m";
+
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        //вводим числа и отображаем их пользователю
+        System.out.println(BLUE + "-+*/Простой калькулятор целых чисел/*+-" +
+                RED
+                + "\nЧисла и произведения должны лежать в границах от -2147483648 до 2147483647"
+                + RESET);
+        System.out.println("Введите первое число = ");
+        try {
+            int number1 = new Scanner(System.in).nextInt();
+            System.out.println("Введите второе число = ");
+            int number2 = new Scanner(System.in).nextInt();
+
+            //Выполняем математические действия
+            System.out.println(YELLOW + "Вычисление..." + RESET);
+            int sum = number1 + number2;
+            System.out.println("Сумма(+) чисел = " + sum);
+            int diff = number1 - number2;
+            System.out.println("Разность(-) чисел = " + diff);
+            int multi = number1 * number2;
+                System.out.println("Произведение(*) чисел = " + multi);
+            //Хотел описать что int переполнен и значение пошло от отрицательной границы
+//            else {
+//                System.out.println("Похоже, что произошло перепополнение");
+//            }
+            double division = (double) number1 / number2;
+            //если частное целое, то отображаем только целое значение
+            if (division % 1 == 0) {
+                System.out.println("Частное чисел(/) = " + (int) division);
+            } else {
+                System.out.println("Частное чисел(/) = " + division);
+            }
+        } catch (InputMismatchException e) {
+            System.out.println(RED
+                    + "Числа и произведения должны лежать в границах от -2147483648 до 2147483647" +
+                    "\nПерезапустите программу"
+                    + RESET);
+        }
     }
 }
