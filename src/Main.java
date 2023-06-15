958760dd582ab328d3b49752efea4ee55515bd3c
Alexandr Suzdaltsev <ASuzdaltsev@inno.tech>
2023-06-15 14:10:33 +0300

    Checked task, deleted unnecessary logic

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int countFiles = 0;
+        String path;
+        File file;
+        boolean isItAFile;
+        boolean isItADirectory;
         while (true) {
-            String path;
-            File file;
-            boolean isItAFile = false;
-            boolean isItADirectory = false;
-            while ((!isItAFile) && (!isItADirectory)) {
                 System.out.println("Введите адрес файла:");
                 path = new Scanner(System.in).nextLine();
                 file = new File(path);
                 isItAFile = file.exists();
-                if (isItAFile) {
-                    isItADirectory = file.isDirectory();
-                    if (isItADirectory) {
-                        System.out.println("Указанный путь является путем к папке, а не к файлу");
-                        isItAFile = false;
-                        isItADirectory = false;
-                    }
+                isItADirectory = file.isDirectory();
+                if (!isItAFile){
+                    System.out.println("Файл не существует");
+                    continue;
                 }
-                System.out.println("Файл не существует");
-            }
+                if (isItADirectory) {
+                    System.out.println("Указанный путь является путем к папке, а не к файлу");
+                    continue;
+                }
             System.out.println("Путь указан верно");
             countFiles++;
             System.out.println("Это файл номер " + countFiles);
